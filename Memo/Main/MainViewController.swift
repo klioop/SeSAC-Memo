ba2fb497e311612f944a32527d9080358134c2bf
Sam <klioop2@gmail.com>
2021-11-08 17:58:30 +0900

    Initial Setting

---
+++ Memo/Main/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Memo
+//
+//  Created by klioop on 2021/11/08.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .always
+    
+    }
+    
+}
