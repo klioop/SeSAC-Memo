bcc6ec91dbaa6a44147a500a6caaa72f708463e3
Sam <klioop2@gmail.com>
2021-11-08 18:06:59 +0900

    title view

--- Memo/Main/MainViewController.swift
+++ Memo/Main/MainViewController.swift
@@ -11,8 +11,23 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.largeTitleDisplayMode = .always
+        setUpTitleView()
+    }
     
+    func setUpTitleView() {
+        let titleView =  UIView(
+                frame: CGRect(
+                    x: 0,
+                    y: 0,
+                    width: view.bounds.width,
+                    height: 100
+                )
+            )
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: titleView.frame.width - 20, height: titleView.frame.height))
+        label.text = "1,234 개의 메모"
+        label.font = .systemFont(ofSize: 40, weight: .bold)
+        titleView.addSubview(label)
+        navigationItem.titleView = titleView
     }
     
 }
