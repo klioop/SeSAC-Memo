36931cc223eaff212a957d812cd3f45c3cda4a82
Sam <klioop2@gmail.com>
2021-11-10 00:11:21 +0900

    PersistanceManager - 메모 로드 함수 수정, 메모 고정, 고정해제 함수 생성

--- Memo/Manager/PersistanceManager.swift
+++ Memo/Manager/PersistanceManager.swift
@@ -11,13 +11,17 @@ import RealmSwift
 class PersistanceManager {
     
     enum RealmError: Error {
-        case faildToCreateMemo
-        case faildToDeleteMemo
+        case failedToCreateMemo
+        case failedToDeleteMemo
+        case failedToFixMemo
+        case failedToUnFixMemo
         
         var errorMessage: String {
             switch self {
-            case .faildToCreateMemo: return "메모 생성에 실패하였습니다."
-            case .faildToDeleteMemo: return "메모 삭제에 실패하였습니다."
+            case .failedToCreateMemo: return "메모 생성에 실패하였습니다."
+            case .failedToDeleteMemo: return "메모 삭제에 실패하였습니다."
+            case .failedToFixMemo: return "메모 고정 실패"
+            case .failedToUnFixMemo: return "메모 고정 해제 실패"
             }
         }
     }
@@ -32,13 +36,39 @@ class PersistanceManager {
                 localRealm.add(memo)
             }
         } catch {
-            let error = RealmError.faildToCreateMemo
+            let error = RealmError.failedToCreateMemo
             print(error.errorMessage)
         }
     }
     
-    func loadAllMemos() -> Results<MemoObject>{
-        localRealm.objects(MemoObject.self).sorted(byKeyPath: "dateEditted", ascending: false)
+    func loadAllNonFixedMemos() -> Results<MemoObject> {
+        localRealm.objects(MemoObject.self).sorted(byKeyPath: "dateEditted", ascending: false).filter("isFixed == false")
+    }
+    
+    func loadAllFixedMemos() -> Results<MemoObject> {
+        localRealm.objects(MemoObject.self).sorted(byKeyPath: "dateEditted", ascending: false).filter("isFixed == true")
+    }
+    
+    func fixMemo(_ memo: MemoObject) {
+        do {
+            try localRealm.write {
+                memo.isFixed = true
+            }
+        } catch {
+            let error = RealmError.failedToFixMemo
+            print(error.errorMessage)
+        }
+    }
+    
+    func unFixMemo(_ memo: MemoObject) {
+        do {
+            try localRealm.write {
+                memo.isFixed = false
+            }
+        } catch {
+            let error = RealmError.failedToUnFixMemo
+            print(error.errorMessage)
+        }
     }
     
     func deleteMemo(_ memo: MemoObject) {
@@ -47,8 +77,8 @@ class PersistanceManager {
                 localRealm.delete(memo)
             }
         } catch {
-            let error = RealmError.faildToDeleteMemo
-            print(error)
+            let error = RealmError.failedToDeleteMemo
+            print(error.errorMessage)
         }
     }
     
